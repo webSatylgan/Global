ab9a2d60553d855ed5392c1d19258af3fbf5edf5
Danial-github <82264689+Danial-github@users.noreply.github.com>
2021-07-02 15:47:48 +0600

    Global

---
+++ script.js
@@ -0,0 +1,74 @@
+
+$('a').on('click', function(e){
+    e.preventDefault();
+    
+});
+
+const swiper = new Swiper('.slider', {
+
+    direction: 'vertical',
+    slidesPerView: 1,
+    loop: true,
+    speed: 700,
+
+    mousewheel: {
+        invert: true,
+    },
+
+});
+
+const workSlider = new Swiper('.works__slider', {
+
+    slidesPerView: 3,
+    loop: true,
+    centeredSlides: true,
+
+    navigation: {
+        nextEl: '.arrow-next',
+        prevEl: '.arrow-prev',
+    },
+
+});
+
+$('.header__btn').on('click', function(){
+    $('.slider').addClass('transform');
+    $('.nav').addClass('active');
+    $('.header').addClass('opacity');
+
+    let Navid = $('.swiper-slide-active').data('nav');
+    $(Navid).addClass('active');
+});
+
+$('.nav a').on('click', function(e){
+    e.preventDefault();
+    $('.nav a').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).data('index');
+
+
+    swiper.slideTo(index, 700)
+
+    setTimeout(function(){
+        $('.slider').removeClass('transform');
+        $('.nav').removeClass('active');
+        $('.header').removeClass('opacity');
+    }, 800);
+});
+
+swiper.on('transitionStart', function(){
+    $('.slider__pagination p').removeClass('active');
+    let id = $('.swiper-slide-active').data(id);
+    $(id).addClass('active');
+    
+});
+
+
+$('.slider__pagination p').on('click', function(){
+    $('.slider__pagination p').removeClass('active');
+    $(this).addClass('active');
+
+    let index = $(this).data('index');
+    swiper.slideTo(index, 700);
+    
+});
